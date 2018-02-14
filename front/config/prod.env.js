7999a07e7851af4953ada839660d8ad6e5b81fa0
Alexis Tacnet <alexistacnet@gmail.com>
2018-02-14 02:01:11 +0100

    Fix production config again

--- front/config/prod.env.js
+++ front/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  AUTH_URL: '"https://api.bpleague.cs-campus.fr/auth"',
-  API_URL: '"https://api.bpleague.cs-campus.fr/api/v1"'
+  AUTH_URL: '"https://api-bpleague.cs-campus.fr/auth"',
+  API_URL: '"https://api-bpleague.cs-campus.fr/api/v1"'
 }
