a24c66c1e397b8ea29a030ede25820f3be02600f
Alexis Tacnet <alexistacnet@gmail.com>
2018-02-14 01:33:41 +0100

    Add production assets

--- front/config/prod.env.js
+++ front/config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  AUTH_URL: '"http://bpleague.cs-campus.fr:5000/auth"',
+  API_URL: '"http://bpleague.cs-campus.fr:5000/api/v1"'
 }
